0b77d15034f33d2975174a8dee993f61b67f283d
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-02-18 21:43:12 +0530

    Updated Code

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -6,14 +6,14 @@ import { PiStudentBold } from "react-icons/pi";
 
 function Dashboard(props) {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="flex justify-between items-center p-5">
+    <div className="min-h-screen bg-gray-50">
+      <div className="flex bg-white border-b border-b-gray-200 justify-between items-center p-5">
         <p className="text-2xl font-semibold text-black">Dashboard</p>
         <p className="text-red-500 font-semibold cursor-pointer">Logout</p>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-5 gap-y-10 mt-24 p-5 bg-grid-pattern">
-        <div className="flex flex-col gap-y-4 text-center border border-gray-200 items-center shadow-md w-fit p-5 rounded-md">
+        <div className="flex flex-col gap-y-4 text-center bg-white border border-gray-200 items-center shadow-md w-fit p-5 rounded-md">
           <div className="bg-pink-100 w-fit p-5 rounded-xl shadow-md">
             <SlCalender />
           </div>
@@ -21,7 +21,7 @@ function Dashboard(props) {
             <p className="font-semibold">Daily Attendance</p>
           </div>
         </div>
-        <div className="flex flex-col gap-y-4 border border-gray-200 items-center shadow-md p-5 rounded-md">
+        <div className="flex flex-col gap-y-4 bg-white border border-gray-200 items-center shadow-md p-5 rounded-md">
           <div className="bg-pink-100 w-fit p-5 rounded-xl shadow-md">
             <TbReportAnalytics />
           </div>
@@ -29,13 +29,13 @@ function Dashboard(props) {
             <p className="font-semibold">Analytics</p>
           </div>
         </div>
-        <div className="flex flex-col gap-y-4 border border-gray-200 items-center text-center shadow-md p-5 rounded-md">
+        <div className="flex flex-col gap-y-4 bg-white border border-gray-200 items-center text-center shadow-md p-5 rounded-md">
           <div className="bg-pink-100 p-5 rounded-xl shadow-md">
             <PiStudentBold />
           </div>
           <p className="font-semibold">Student Attendance</p>
         </div>
-        <div className="flex flex-col gap-y-4 border border-gray-200 items-center text-center shadow-md p-5 rounded-md">
+        <div className="flex flex-col gap-y-4 bg-white border border-gray-200 items-center text-center shadow-md p-5 rounded-md">
           <div className="bg-pink-100 w-fit p-5 rounded-xl shadow-md">
             <SiGoogleclassroom />
           </div>
@@ -45,7 +45,7 @@ function Dashboard(props) {
         </div>
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 text-black p-4 shadow-lg">
+      <div className="fixed bottom-0 bg-white border-t border-t-gray-200 left-0 right-0 text-black p-4 shadow-lg">
         <div className="grid grid-cols-4 gap-4 text-center">
           <div className="flex flex-col items-center">
             <SlCalender className="text-2xl" />
